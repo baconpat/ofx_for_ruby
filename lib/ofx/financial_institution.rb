465f74e0692f3904edce69e672184f9ae77630b9
Brian Welty <bkwelty@zoho.com>
2014-01-22 22:41:59 -0800

    Update FinancialInstiution with support for AMEX and Chase. Expand FinancialInstitution with a large set of helper functions, such that user can pull OFX data by only interacting with this one class.

--- lib/ofx/financial_institution.rb
+++ lib/ofx/financial_institution.rb
@@ -20,16 +20,32 @@ require 'uri'
 module OFX
     class FinancialInstitution
 
-        def self.get_institution(financial_institution_name)
+        def self.get_institution(financial_institution_name, ofx_client_id=nil, ssl_version=nil)
             case financial_institution_name
                 when 'Capital One'
                     FinancialInstitution.new('Capital One',
-                                             URI.parse('https://onlinebanking.capitalone.com/scripts/serverext.dll'),
-                                             OFX::Version.new("1.0.2"))
+                                             URI.parse('https://onlinebanking.capitalone.com/ofx/process.ofx'),
+                                             OFX::Version.new("1.0.2"),
+                                             'Hibernia', '1001', '065002030',
+                                             ofx_client_id, ssl_version)
                 when 'Citi'
                     FinancialInstitution.new('Citi',
-                                             URI.parse('https://secureofx2.bankhost.com/citi/cgi-forte/ofx_rt?servicename=ofx_rt&pagename=ofx'),
-                                             OFX::Version.new("1.0.2"))
+                                             URI.parse('https://www.accountonline.com/cards/svc/CitiOfxManager.do'),
+                                             OFX::Version.new("1.0.2"),
+                                             'Citigroup', '24909', nil,
+                                             'Quicken', ssl_version)
+                when 'Chase'
+                    FinancialInstitution.new('Chase',
+                                             URI.parse('https://ofx.chase.com'),
+                                             OFX::Version.new("1.0.2"),
+                                             'B1', '10898', nil,
+                                             'Quicken', :SSLv3)
+                when 'AMEX'
+                    FinancialInstitution.new('AMEX',
+                                             URI.parse('https://online.americanexpress.com/myca/ofxdl/desktop/desktopDownload.do?request_type=nl_ofxdownload'),
+                                             OFX::Version.new("1.0.2"),
+                                             'AMEX', '3101', nil,
+                                             'Quicken', ssl_version)
                 else
                     raise NotImplementedError
             end
@@ -38,11 +54,35 @@ module OFX
         attr :name
         attr :ofx_uri
         attr :ofx_version
+        attr :organization_name
+        attr :organization_id
+        attr :bank_identifier
+        attr :client
 
-        def initialize(name, ofx_uri, ofx_version)
+        def initialize(name, ofx_uri, ofx_version, org_name, org_id, bank_id=nil, client_id=nil, ssl_version=nil)
             @name = name
             @ofx_uri = ofx_uri
             @ofx_version = ofx_version
+            @organization_name = org_name
+            @organization_id = org_id
+            @bank_identifier = bank_id
+            @client = nil
+            @ofx_client_id = client_id
+            @ofx_ssl_version = ssl_version
+        end
+
+        def set_client(user_name, password)
+          inst_id = OFX::FinancialInstitutionIdentification.new(
+                            @organization_name, @organization_id)
+          user_cred = OFX::UserCredentials.new(user_name, password)
+          @client = OFX::FinancialClient.new([[inst_id, user_cred]])
+        end
+
+        # anonymous can be used for ProfileRequest
+        def set_client_anon
+          user_name = "anonymous00000000000000000000000"
+          password = "anonymous00000000000000000000000"
+          set_client(user_name, password)
         end
 
         def create_request_document()
@@ -58,16 +98,99 @@ module OFX
             end
 
             document.header.security = "NONE"
-
             document.header.content_encoding = "USASCII"
             document.header.content_character_set = "1252"
-
             document.header.compression = "NONE"
-
             document.header.previous_unique_identifier = "NONE"
             document.header.unique_identifier = OFX::FileUniqueIdentifier.new
 
             document
+        end
+
+        def create_request_document_signon
+          return nil if @client.nil?
+          requestDocument = self.create_request_document
+          requestDocument.message_sets << @client.create_signon_request_message(@organization_id, @ofx_client_id)
+          return requestDocument
+	end
+
+        def create_request_document_profile_update(request_date=nil)
+          return nil if @client.nil?
+          if request_date.nil?
+            request_date = DateTime.new(2001, 1, 1)
+          end
+          profileMessageSet = OFX::FinancialInstitutionProfileMessageSet.new
+          profileRequest = OFX::FinancialInstitutionProfileRequest.new
+          profileRequest.transaction_identifier = OFX::TransactionUniqueIdentifier.new
+          profileRequest.client_routing = 'MSGSET'
+          profileRequest.date_of_last_profile_update = request_date
+          profileMessageSet.requests << profileRequest
+
+          requestDocument = self.create_request_document
+          requestDocument.message_sets << @client.create_signon_request_message(@organization_id, @ofx_client_id)
+          requestDocument.message_sets << profileMessageSet
+          return requestDocument
+	end
+
+        def create_request_document_signup(request_date=nil)
+          return nil if @client.nil?
+          if request_date.nil?
+            request_date = DateTime.new(2001, 1, 1)
+          end
+          signup_message_set = OFX::SignupMessageSet.new
+          account_info_request = OFX::AccountInformationRequest.new
+          account_info_request.transaction_identifier = OFX::TransactionUniqueIdentifier.new
+          account_info_request.date_of_last_account_update = request_date
+          signup_message_set.requests << account_info_request
+
+          requestDocument = self.create_request_document
+          requestDocument.message_sets << @client.create_signon_request_message(@organization_id, @ofx_client_id)
+          requestDocument.message_sets << signup_message_set
+          return requestDocument
+        end
+
+        def create_request_document_for_cc_statement(account_id, date_range=nil, include_trans=true)
+          return nil if @client.nil?
+          cc_message_set = OFX::CreditCardStatementMessageSet.new
+          statement_request = OFX::CreditCardStatementRequest.new
+          statement_request.transaction_identifier = OFX::TransactionUniqueIdentifier.new
+          statement_request.account = OFX::CreditCardAccount.new
+          statement_request.account.account_identifier = account_id
+          if include_trans
+            statement_request.included_range = date_range
+            statement_request.include_transactions = include_trans
+          end
+          cc_message_set.requests << statement_request
+
+          requestDocument = self.create_request_document
+          requestDocument.message_sets << @client.create_signon_request_message(@organization_id, @ofx_client_id)
+          requestDocument.message_sets << cc_message_set
+          return requestDocument
+	end
+
+        def create_request_document_for_cc_closing_statement(account_id)
+          create_request_document_for_cc_statement(account_id, nil, false)
+        end
+
+        def create_request_document_for_bank_statement(account_id, date_range=nil, account_type = :checking)
+          return nil if @client.nil?
+          banking_message_set = OFX::BankingMessageSet.new
+          statement_request = OFX::BankingStatementRequest.new
+          statement_request.transaction_identifier = OFX::TransactionUniqueIdentifier.new
+          statement_request.account = OFX::BankingAccount.new
+          statement_request.account.bank_identifier = @bank_identifier
+          statement_request.account.branch_identifier = nil
+          statement_request.account.account_identifier = account_id
+          statement_request.account.account_type = account_type
+          statement_request.account.account_key = nil
+          statement_request.include_transactions = true
+          statement_request.included_range = date_range  # example DateRange (start.to_date)..(end.to_date)
+          banking_message_set.requests << statement_request
+
+          requestDocument = self.create_request_document
+          requestDocument.message_sets << @client.create_signon_request_message(@organization_id, @ofx_client_id)
+          requestDocument.message_sets << banking_message_set
+          return requestDocument
         end
 
         def send(document)
@@ -75,8 +198,34 @@ module OFX
             request_body = serializer.to_http_post_body(document)
 
             client = OFX::HTTPClient.new(@ofx_uri)
-            response_body = client.send(request_body)
+            response_body = client.send(request_body, @ofx_ssl_version)
 
+            return serializer.from_http_response_body(response_body)
+        end
+
+##
+## Debugging routines
+##
+        def get_anon_profile
+            set_client_anon
+            return send(create_request_document_profile_update)
+        end
+
+        def document_to_post_data(document, no_whitespace=false)
+            serializer = OFX::Serializer.get(@ofx_version)
+            request_body = serializer.to_http_post_body(document)
+            if no_whitespace
+                request_body.delete! " "
+            end
+            return request_body
+        end
+
+        def send_from_post_data(data, debug_req=false, debug_resp=false)
+            client = OFX::HTTPClient.new(@ofx_uri)
+            request_body = data
+            response_body = client.send(request_body, @ofx_ssl_version, debug_req, debug_resp)
+
+            serializer = OFX::Serializer.get(@ofx_version)
             return serializer.from_http_response_body(response_body)
         end
     end
