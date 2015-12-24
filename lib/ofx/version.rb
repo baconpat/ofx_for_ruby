27d7cf0b87738ec3255f2d249a804e76dc59b519
Patrick Bacon <bacon@atomicobject.com>
2015-12-23 19:57:21 -0500

    Bumped library version to 0.1.3

--- lib/ofx/version.rb
+++ lib/ofx/version.rb
@@ -1,17 +1,17 @@
 # Copyright © 2007 Chris Guidry <chrisguidry@gmail.com>
 #
 # This file is part of OFX for Ruby.
-# 
+#
 # OFX for Ruby is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # OFX for Ruby is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,9 +21,9 @@ module OFX
 
         def initialize(version)
             @version = [0, 0, 0]
-            
+
             return if version.nil?
-            
+
             parts = case version
             when Version
               version.to_a
@@ -74,13 +74,13 @@ module OFX
         end
         def eql?(other)
             other_a = other.to_a
-            
+
             return @version[0] == other_a[0] && @version[1] == other_a[1] && @version[2] == other_a[2]
         end
         def hash()
             return @version.hash
         end
     end
-    
-    VERSION = Version.new('0.1.2')
+
+    VERSION = Version.new('0.1.3')
 end
